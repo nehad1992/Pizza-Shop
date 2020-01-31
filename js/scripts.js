520857ae428d89cc3a218608d57e034486973be5
neha doodi <nehadoodipoonia@gmail.com>
2020-01-31 12:37:06 -0800

    add checkboxes & take multiple checkboxes as input

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 // Business Logic
 // Constructor for Pizza
-// function Pizza(size, base, cheese, sauce, meat, veggies) {
-//   this.size = size;
-//   this.base = base;
-//   this.cheese = cheese;
-//   this.sauce = sauce;
-//   this.meat = meat;
-//   this.veggies = veggies;
-// }
-// Pizza.prototype.chooseSize = function () {
+function Pizza(size, base, cheese, sauce, meat, veggies) {
+  this.size = size;
+  this.base = base;
+  this.cheese = cheese;
+  this.sauce = sauce;
+  this.meat = meat;
+  this.veggies = veggies;
+}
+Pizza.prototype.cost = function () {
 
-// }
+}
 
 
 
@@ -23,7 +23,18 @@ $(document).ready(function () {
   $("#pizza").submit(function (event) {
     event.preventDefault();
     var size = $("input:radio[name=size]:checked").val();
-    console.log(size);
+    var base = $("input:radio[name=base]:checked").val();
+    var cheese = $("input:radio[name=cheese]:checked").val();
+    var sauce = $("input:radio[name=sauce]:checked").val();
+    var protein = [];
+    $.each($("input[name='protein']:checked"), function () {
+      protein.push($(this).val());
+    });
+    var veggies = [];
+    $.each($("input[name='veggies']:checked"), function () {
+      veggies.push($(this).val());
+    });
+    console.log(veggies);
 
 
 
