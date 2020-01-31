728be3ac593e3662689449c7ffb75b4ca033685f
neha doodi <nehadoodipoonia@gmail.com>
2020-01-31 14:06:44 -0800

    add constructor, prototype for size cost & cheese cost

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,51 @@
 // Business Logic
 // Constructor for Pizza
-function Pizza(size, base, cheese, sauce, meat, veggies) {
+function Pizza(size, base, cheese, sauce, protein, veggies) {
   this.size = size;
   this.base = base;
   this.cheese = cheese;
   this.sauce = sauce;
-  this.meat = meat;
+  this.protein = protein;
   this.veggies = veggies;
-}
-Pizza.prototype.cost = function () {
-
+  this.cost = 0;
 }
 
+function Hybrid(text, value) {
+  this.text = text;
+  this.value = value;
+}
 
+// Calculate cost of size
+Pizza.prototype.sizeCost = function () {
+  if (this.size === "small") {
+    this.cost += parseInt(2);
+  } else if (this.size === medium) {
+    this.cost += parseInt(3);
+  } else if (this.size === larger) {
+    this.cost += parseInt(4);
+  } else {
+    this.cost += parseInt(5);
+  }
+}
+// Calculate cost of cheese
+Pizza.prototype.cheeseCost = function () {
+  if (this.cheese = noCheese) {
+    this.cheese = parseInt(0);
+  } else if (this.cheese = lightCheese) {
+    this.cheese = parseInt(1);
+  } else {
+    this.cheese = parseInt(2);
+  }
+}
+
+// Calculate cost of Base
+Pizza.prototype.baseCost = function () {
+
+}
+
+Pizza.prototype.totalCost = function () {
+  this.sizeCost();
+}
 
 
 
@@ -34,7 +67,9 @@ $(document).ready(function () {
     $.each($("input[name='veggies']:checked"), function () {
       veggies.push($(this).val());
     });
-    console.log(veggies);
+    var pizza = new Pizza(size, base, cheese, sauce, protein, veggies);
+    pizza.totalCost();
+    console.log(pizza.cost);
 
 
 
